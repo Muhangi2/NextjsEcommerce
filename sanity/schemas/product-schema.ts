14db80ff9d701066c6e6bd454fb8c15ab03263b6
Muhangi2 <eliodamutiba@gmail.com>
2024-02-16 23:40:13 +0300

    sc

--- sanity/schemas/product-schema.ts
+++ sanity/schemas/product-schema.ts
@@ -1,1 +1,31 @@
-export const product = {}
+import { defineField,defineType } from "sanity"
+
+export const product = defineType({
+    name: "product",
+    title: "Product",
+    type: "document",
+    fields:[
+        defineField({
+            name:"name",
+            title:"Name",
+            type:"string",
+        } ),{
+            name:"slug",
+            title:"Slug",
+            type:"slug",
+            options:{
+                source:"name",
+            }
+        },{
+            name:"images",
+            title:"Images",
+            type:"array",
+            of:[{type:"image"}],
+        },{
+            name:"categories",
+            title:"Categories",
+            type:"array",
+        }
+    
+    ]
+})
